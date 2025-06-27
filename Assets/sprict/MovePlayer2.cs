82e9139438a31de046bb9ee8e6858ed4a28bba44
顔文字信仰者（saise173） <mouhuwokiwamesimono@gmail.com>
2025-06-27 21:29:46 +0900

    6/27 player2変更点
    mainで言っていたように
    player2の左右の移動も加速度による運動にしました。
    プレイヤーが回転しないようにしました
    ジャンプ回数や上下、左右の移動倍率などを変更できるようにしました。

--- Assets/sprict/MovePlayer2.cs
+++ Assets/sprict/MovePlayer2.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class MovePlayer2 : MonoBehaviour
 {
-    [SerializeField, Header("移動速度")]
-    private float speedBoost = 1;
+    [SerializeField, Header("左右の移動速度倍率")]private float right_and_left_speedBoost = 1;
+
+    [SerializeField, Header("上下の移動速度倍率")]private float junp_speedBoost = 1;
+
+    [SerializeField, Header("ジャンプ回数")]private int can_junp_count = 1;
     private float speed = 0.05f;
 
-    private bool isGround = true;
+    private int isGround;
 
     private Rigidbody2D rb;
 
     private SpriteRenderer player1_renderer;
 
+    [SerializeField, Header("左右の最大速度(正の値でお願いします)")] private float right_and_left_max_speed = 10;
+
     [SerializeField, Header("下方向の最大速度(負の値でお願いします)")] private float donw_max_speed = -10;
 
     void Start()
     {
+        float right_max_speed = right_and_left_max_speed;
+        float left_max_speed = -right_and_left_max_speed;
         rb = GetComponent<Rigidbody2D>();
         player1_renderer = GetComponent<SpriteRenderer>();
+        isGround = can_junp_count;
     }
 
     void Update()
@@ -27,12 +35,19 @@ public class MovePlayer2 : MonoBehaviour
         Vector2 position = transform.position;
         if (Input.GetKey("left"))
         {
-            position.x -= speed * speedBoost;
+            if (rb.linearVelocityX >-right_and_left_max_speed) {
+                rb.AddForce(new Vector2(-0.5f * right_and_left_speedBoost, 0), ForceMode2D.Impulse);
+                //position.x -= speed * speedBoost;
+            }
             player1_renderer.flipX = true;
         }
         if (Input.GetKey("right"))
-        {
-            position.x += speed * speedBoost;
+        {   
+            if (rb.linearVelocityX < right_and_left_max_speed) {
+                rb.AddForce(new Vector2(0.5f * right_and_left_speedBoost, 0), ForceMode2D.Impulse);
+                //position.x -= speed * speedBoost;
+            }
+            //position.x += speed * speedBoost;
             player1_renderer.flipX = false;
         }
 
@@ -41,11 +56,11 @@ public class MovePlayer2 : MonoBehaviour
             player1_renderer.flipY = true;
             if (rb.linearVelocityY > donw_max_speed)
             {
-                rb.AddForce(new Vector2(0, -2f * speedBoost), ForceMode2D.Impulse);
+                rb.AddForce(new Vector2(0, -2f * junp_speedBoost), ForceMode2D.Impulse);
             }
-            else if(donw_max_speed<0)
+            else if (donw_max_speed < 0)
             {
-                rb.linearVelocityY = donw_max_speed;
+                //rb.linearVelocityY = donw_max_speed;
             }
         }
         else
@@ -53,20 +68,20 @@ public class MovePlayer2 : MonoBehaviour
             player1_renderer.flipY = false;
         }
 
-        if (Input.GetKeyDown("up"))//&& isGround
+        if (Input.GetKeyDown("up") && isGround > 0)//&& isGround
         {
-            rb.AddForce(new Vector2(0, 5f * speedBoost), ForceMode2D.Impulse);
-            isGround = false;
+            rb.AddForce(new Vector2(0, 5f * junp_speedBoost), ForceMode2D.Impulse);
+            isGround -= 1;
         }
 
         transform.position = position;
     }
 
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if (collision.gameObject.CompareTag("floor"))
-    //     {
-    //         isGround = true;
-    //     }
-    // }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("floor"))
+        {
+            isGround = can_junp_count;
+        }
+    }
 }
